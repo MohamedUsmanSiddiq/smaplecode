557b1358dde0368c01aad561b3c381e6d658cf06
MohamedUsmanSiddiq <usmansiddiq0204@gmail.com>
2024-05-27 22:19:47 +0530

    changedcode

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-function employee(age){
-        return age;
+students = [
+    { id: 1, name: "mohamed" },
+    { id: 2, name: "usman" },
+    { id: 3, name: "siddiq" },
+    { id: 4, name: "otis" },
+    { id: 5, name: "anto"},
+]
+
+schoolAstudentIds = [1, 3, 5]
+schoolBstudentIds = [2, 4]
+
+function print(IDS) {
+    let i = 0;
+    while (i < IDS.length) {
+        const studentId = IDS[i];
+        //console.log("studentID= "+studentId)
+        let j = 0;
+        while (j < students.length) {
+            const jStudent = students[j];
+            if (studentId === jStudent.id) {
+                console.log(jStudent);
+            }
+            j++
+        }
+        i++
+    }
 }
-const employeeAge=employee(20);
-console.log(employeeAge);
+print(schoolAstudentIds);
+
+
+
+
